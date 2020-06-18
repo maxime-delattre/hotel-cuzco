6b1a9d90b05957d386e3a51e8a9a0557710bde06
amha <amira.halaoua@accenture.com>
2020-06-18 15:19:07 +0200

    add test for room class

---
+++ src/main/java/hotel/Application.java
@@ -0,0 +1,10 @@
+package hotel;
+
+import hotel.domain.*;
+
+public class Application {
+    public static void main(String[] args) {
+        // Room room = new Room();
+        // System.out.println(Room.toString());
+        }
+}
